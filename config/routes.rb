62ed51aa9d37daafe5a17734827435ed1f8934dd
Yuki <github@risaton.net>
2024-01-30 17:33:48 +0900

    add controller mypage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'top', to: 'top#index'
   get 'signup', to: 'signup#new'
   post 'signup', to: 'signup#create'
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
