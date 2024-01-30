21a850bb5d2dc2f4da7a40ccc8fd2c180b729701
Yuki <github@risaton.net>
2024-01-30 22:36:07 +0900

    commit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,6 +10,12 @@ class SignupController < ApplicationController
       render :new
     end
   end
+  def show
+    @user = User.find(params[:email])
+  end
+  def destroy
+    @user = User.find(params[:email])
+  end
 
   def destroy_params
     params.require(:email)
