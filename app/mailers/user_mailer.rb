d2f17003972f1fb31594a7b291f37437e0c5bcc4
Yuki <github@risaton.net>
2024-01-30 15:42:34 +0900

    cleanup

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "lisaton@risaton.net"
-  def test_email
-    @user = params[:user]
-    mail(to: 'lisaton@risaton.net', subject: 'Testing from Rails!')
-    UserMailer.with(user: @user).test_email.deliver_now
-  end
-end
