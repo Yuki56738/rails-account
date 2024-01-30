2e126732dd2481b2d5cade0b22296ef8dddb18a7
Yuki <github@risaton.net>
2024-01-30 15:52:03 +0900

    add controller session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # users = User.all
+  def logged_in?
+    !!session[:user_name]
+  end
+  def current_user
+    @current_user ||= User.find_by(session[:user_name]) if !!session[:user_name]
+  end
 end
