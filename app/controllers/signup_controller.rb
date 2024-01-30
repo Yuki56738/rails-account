2ff43d1a48c70db2e9ddcf552cd13f36f71182d4
Yuki <github@risaton.net>
2024-01-30 20:11:49 +0900

    commit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,6 +10,19 @@ class SignupController < ApplicationController
       render :new
     end
   end
+  def destroy
+    @user = current_user # 仮にcurrent_userメソッドが現在ログインしているユーザーを返すと仮定
+
+    if @user.destroy
+      reset_session # ユーザーを削除した後はセッションをリセット
+      redirect_to root_path, notice: 'アカウントを削除しました。'
+    else
+      redirect_to root_path, alert: 'アカウントの削除に失敗しました。'
+    end
+  end
+  def destroy_params
+    params.require(:email)
+  end
   def user_params
     params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
 
